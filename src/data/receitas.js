71d3ee91e2184f32b918ef975d9522de06551dca
amrithomas <amri.righetti@gmail.com>
2023-09-26 16:17:32 -0300

    Create receitas.js

---
+++ src/data/receitas.js
@@ -0,0 +1,320 @@
+export const categories = [
+    {
+      id: 1,
+      name: "Bovinas",
+    },
+    {
+      id: 2,
+      name: "Suinas",
+    },
+    {
+      id: 3,
+      name: "Frango",
+    },
+  ];
+  
+  export const recipes = [
+    {
+      recipeId: 0,
+      categoryId: 1,
+      title: "Fraldinha na pressão",
+      photo_url:
+        "https://www.receitasmais.com.br/wp-content/uploads/2022/11/IMG_5834-768x1024.jpeg",
+      time: "60",
+      ingredients: [
+        []
+      ],
+      description:
+        "-- Passe sal fino em volta da fraldinha e reserve. \n\n -- Corte em rodelas grossas as cebolas e as batatas. \n\n -- Pegue uma panela de pressão adicione um fio de azeite e coloque a fraldinha com a gordura para baixo. \n\n -- Cubra com as rodelas de cebola e de batatas. \n\n -- Em seguida adicione uma pitada de pimenta-do-reino, 1 pitada de pimenta calabresa, o alho bem picadinho, 1 pitada de sal e o azeite restante. \n\n -- Tampe a panela e leve em fogo médio por 40 minutos (não é necessário colocar água). \n\n -- Após isso desligue o fogo e deixe a pressão da panela sair normalmente. \n\n -- Destampe a panela e adicione a cebolinha. \n\n -- Em seguida coloque a fraldinha em um refratário, decore e sirva.",
+    },
+    {
+      recipeId: 1,
+      categoryId: 1,
+      title: "Contrafilé acebolado",
+      photo_url: "https://i.ytimg.com/vi/HJJ1Xj8RHuQ/hqdefault.jpg",
+      time: "5",
+      ingredients: [[]],
+      description:
+        "-- Tempere os bifes e reserve. \n\n -- Em uma frigideira grande, derreta a manteiga e coloque os bifes lado a lado na frigideira, em fogo alto. \n\n -- Em seguida, vire-os para que dourem dos dois lados. \n\n -- Prossiga a fritura até os bifes ficarem no ponto desejado. \n\n -- Reserve-os, mantendo-os aquecidos. \n\n -- Na mesma frigideira, junte a cebola e o vinagre e refogue bem até que as cebolas murchem. \n\n -- Despeje esta mistura sobre os bifes e sirva a seguir.",
+    },
+    {
+      recipeId: 2,
+      categoryId: 1,
+      title: "Estrogonofe de Alcatra",
+      photo_url:
+        "https://colonialconservas.com.br/wp-content/uploads/2022/09/strogonoff-de-alcatra.jpg",
+      time: "25",
+      ingredients: [[]],
+      description:
+        "-- Coloque em uma panela a margarina, as cebolas, os tomates e o alho, leve ao fogo e refogue ficares macios. \n\n -- Retire do fogo e reserve. \n\n -- Na mesma panela aqueça o óleo e frite a carne, sem mexer muito, até dourar. \n\n -- Polvilhe a farinha de trigo junto a carne e doure-a sem parar de mexer. \n\n -- Junte o refogado, o molho ingles, a mostarda, o catchup, o açúcar, o sal, temperos e cozinhe por mais 10 minutos. \n\n -- Acrescente o creme de leite e continue a cozinhar, sem parar de mexer. \n\n -- Ao fever acerto o sal e temperos e retire do fogo.",
+    },
+    {
+      recipeId: 3,
+      categoryId: 1,
+      title: "Costela Assada no Forno",
+      photo_url:
+        "https://anamariabrogui.com.br/assets/uploads/receitas/fotos/usuario-3405-c62bdc3fe7a5c5d6cb23d569cc3900e3.jpg",
+      time: "45",
+      ingredients: [[]],
+      description:
+        "-- Amasse o alho, juntamente com o sal, pimenta, cebola e sazon, passe esse tempero muito bem na costela. \n\n --Envolva a costela já temperada no papel alumínio por três vezes (três folhas de papel alumínio). Deixando a costela em contato com o lado mais brilhante do papel, lembrando de apertar bem as bordas para ficar bem lacrado. \n\n -- Leve ao forno quente por 2 horas. \n\n -- Depois deste tempo retire o papel alumínio e deixe no forno para dar uma douradinha. \n\n -- Fica uma delícia.",
+    },
+    {
+      recipeId: 4,
+      categoryId: 1,
+      title: "Picanha Invertida",
+      photo_url:
+        "https://minervafoods.com/wp-content/uploads/2022/12/picanha_blog.jpg",
+      time: "15",
+      ingredients: [[]],
+      description:
+        "-- Fure a picanha no sentido diagonal, deixando 1,5 cm nas laterais. \n\n -- Vire a ponta para dentro, deixando no avesso, cuidado no corte para não atravessar as laterais de um lado ao outro. \n\n -- Recheie com o provolone picado temperado com orégano e o bacon, também picado. \n\n -- Feche as aberturas com palitos. \n\n -- Salgue com o sal grosso. \n\n -- Coloque no forno a 200ºC em uma assadeira untada ou se preferir leve para churrasqueira. \n\n -- Deixe assar a seu gosto, virando de 15 em 15 minutos.",
+    },
+    {
+      recipeId: 5,
+      categoryId: 2,
+      title: "Pernil a pururuca",
+      photo_url:
+        "https://img.cybercook.com.br/receitas/810/pernil-de-porco-a-pururuca-4.jpeg",
+      time: "60",
+      ingredients: [[]],
+      description:
+        "-- Bater no liquidificador: cebola, alho, vinagre, suco dos limões, sal e pimenta. \n\n -- Coar em um pano ( utilizo um pano de prato limpo ). \n\n -- Colocar o líquido em uma seringa grande e injetar no pernil. \n\n -- A parte pastosa deste tempero, passar sobre o pernil, em especial, na parte da carne. \n\n -- Deixar neste tempero por 48 h. \n\n -- Levar ao forno para assar, com a parte do couro para cima, coberto com papel alumínio ( 1 hora para cada k de carne), em fogo médio. \n\n -- 1 hora antes de retirar do forno, retirar o papel alumínio, deixe em fogo alto, para dourar. \n\n -- Depois de dourado, jogar o óleo fervendo sobre o couro.",
+    },
+    {
+      recipeId: 6,
+      categoryId: 2,
+      title: "Lombo ao molho de maracujá",
+      photo_url:
+        "https://www.saboresajinomoto.com.br/uploads/images/recipes/lombo-assado-com-mel-e-maracuja.jpg",
+      time: 20,
+      ingredients: [[]],
+      description:
+        "-- Em um jarro misture a água filtrada, com o suco maguary de maracujá e o açúcar cristal e reserve. \n\n -- Pegue o lombo, retire o excesso de gordura e deixe-o de molho em água filtrada por 10 minutos. \n\n -- Escorrer a água, furar o lombo com uma faca de ponta e tempera-lo apenas com sal. \n\n -- Na panela de pedra, coloque a manteiga e a cebola picada em tiras. \n\n -- Frite-as até que fique 'queimada nas beiradas'. \n\n -- Coloque o lombo e pingue o suco de maracujá aos poucos até que ele fique cozido. Assim que estiver cozido deixe o suco secar e virar um caldo grosso. Sirva com farofa e arroz.",
+    },
+    {
+      recipeId: 7,
+      categoryId: 2,
+      title: "Bisteca suina bagunçada",
+      photo_url:
+        "https://static.itdg.com.br/images/640-400/6ffa5c71ca5a1ea843f16d9983457f4f/104980-original.jpg",
+      time: 30,
+      ingredients: [[]],
+      description:
+        "-- Amasse o alho e o sal.\n\n -- Tempere as bistecas com o sal e alho, pimenta moída na hora e o suco de meio limão. \n\n -- Pique a batata e a cenoura em cubos e cozinhe no vapor deixando ficar 'al dente'. \n\n -- Frite o bacon sem deixar ficar muito seco e reserve. \n\n -- Separe metade da gordura. \n\n -- Em uma panela grande de fundo grosso frite as bistecas em fogo alto na gordura restante do bacon sem deixar secar muito. Reserve-as. \n\n -- Retire o excesso de óleo da panela e frite as cebolas até dourarem. \n\n -- Junte o pimentão, os tomates cortados em 4 partes, a cenoura, a batata, o bacon e o louro e deixe em fogo alto refogar por 5 minutos cuidando para não queimar. Se necessário acrescente água aos poucos. \n\n -- Na mesma panela junte as bistecas fritas e remexa tudo, deixe por 10 minutos, ou até quase secar. \n\n -- Disponha as bistecas em uma travessa e por cima a mistura da panela (sem a folha de louro).",
+    },
+    {
+      recipeId: 8,
+      categoryId: 2,
+      title: "Linguiça calabresa com batata",
+      photo_url: "https://i.ytimg.com/vi/Ll1IfOb6PG4/hqdefault.jpg",
+      time: 0,
+      ingredients: [[]],
+      description:
+        "-- Doure a cebola e o alho no óleo. \n\n -- Acrescente a linguiça e refogue bem. \n\n -- Coloque o molho de tomate e a batata, o sal e a pimenta-do-reino e a salsa, complete com água até que cubra as batatas. \n\n -- Deixe cozinhar por uns 20 minutos, ou até que as batatas estejam macias.",
+    },
+    {
+      recipeId: 9,
+      categoryId: 3,
+      title: "Coxa creme",
+      photo_url: "https://i.ytimg.com/vi/ayDiAeBRwxM/maxresdefault.jpg",
+      time: 0,
+      ingredients: [[]],
+      description:
+        "-- Coloque as 6 coxas de frango para cozinhar com sal e temperos a gosto (cebola, alho, salsinha), após cozidas reserve uma xícara de chá do caldo coado do cozimento das coxas. \n\n -- Coloque para ferver o caldo das coxas, o creme de leite e a manteiga. \n\n -- Quando ferver acrescente a farinha e mexa sem parar por 2 minutos para que a massa cozinhe. \n\n -- Coloque as gemas, a salsinha e mexa rapidamente. \n\n -- Deixe esfriar um pouco, divida a massa em 6 pedaços e abra cada pedaço na palma das mãos. Coloque uma coxa e modele, de tal modo que a coxa fique coberta, deixando a ponta do osso de fora. \n\n -- Passe pela clara, pela farinha de rosca, aperte bem e frite em óleo quente sob imersão. Escorra, coloque em papel absorvente e sirva ainda quente.",
+    },
+    {
+      recipeId: 10,
+      categoryId: 3,
+      title: "Sobrecoxa de frango na mostarda",
+      photo_url:
+        "https://static.itdg.com.br/images/1200-675/97b6b3cf688618526088d8015a4e77b6/279620-original.jpg",
+      time: 0,
+      ingredients: [[]],
+      description:
+        "-- Lave as sobrecoxas e deixe-as de molho no vinagre. \n\n -- Enxague e deixe escorrer toda a água. \n\n -- Tempere com o sal e a pimenta-do-reino, misture o leite e a mostarda. \n\n -- Deixe marinando por 3 horas. \n\n -- Distribua as sobrecoxas em uma assadeira (não precisa untar), pincele maionese e asse em forno médio por cerca de 2 horas (o leite amolece a carne e retira toda a gordura do frango).",
+    },
+    {
+      recipeId: 11,
+      categoryId: 3,
+      title: "Asa de frango assada no forno",
+      photo_url: "https://i.ytimg.com/vi/5MglYIiPSuc/maxresdefault.jpg",
+      time: 0,
+      ingredients: [[]],
+      description:
+        "-- Em uma forma, coloque as asinhas de frango, despeje o suco de limão, o sal e a pimenta, o tempero em pó para aves e mexa bem. \n\n -- Em seguida, coloque as batatas, os tomates cortados em rodelas, coloque o orégano em todas as batatas e tomates. \n\n -- Regue com o azeite. \n\n -- Cubra com papel alumínio e leve ao forno por 20 minutos. \n\n -- Retire o papel e deixe por 10 minutos para dourar.",
+    },
+    {
+      recipeId: 12,
+      categoryId: 3,
+      title: "Espetinho Misto",
+      photo_url:
+        "https://swiftbr.vteximg.com.br/arquivos/ids/173528-768-768/espetinho-coracao-swift-900g-617745-2.jpg?v=637544554945300000",
+      time: 0,
+      ingredients: [[]],
+      description:
+        "-- Corte as carnes, frango e calabresa em cubos e tempere a seu gosto, deixe curtir bem o tempero, antes de espetar as carnes envolva a carne suína no bacon para fazer um medalhão e espete. \n\n -- Depois coloque um pedaço de cada tipo para fazer seu espetinho misto, sempre intercalando com um pedaço de cebola e pimentão para dar aquele toque especial que fica uma delícia. \n\n -- Depois e só assar na brasa ou em uma churrasqueira elétrica.",
+    },
+  ];
+  
+  export const ingredients = [
+    {
+      ingredientId : 0,
+      name : "",
+      photo_url : ""
+    },
+    {
+      ingredientId : 0,
+      name : "Vinagre",
+      photo_url : "https://dw0jruhdg6fis.cloudfront.net/producao/25030108/G/750.png"
+    },
+    {
+      ingredientId : 0,
+      name : "Cebolinha",
+      photo_url : "https://s.cornershopapp.com/product-images/2009515.png?versionId=qK4QhnN.luLPO68CIH5aqXn4FJ4Hq9Up"
+    },
+    {
+      ingredientId : 0,
+      name : "Azeite",
+      photo_url : "https://images.tcdn.com.br/img/img_prod/673450/azeite_gallo_extra_virgem_vidro_500_ml_2363_1_ba572a2e6eea9b4114b8370eb704afd6.jpg"
+    },
+    {
+      ingredientId : 0,
+      name : "Fraldinha",
+      photo_url : "https://www.casadecarnesbandeirantes.com.br/wp-content/uploads/2021/05/Fraldinha-angus.png"
+    },
+    {
+      ingredientId: 0,
+      name: "Açucar",
+      photo_url:
+        "https://previews.123rf.com/images/sommai/sommai1411/sommai141100034/33199985-sugar-cubes-in-a-bowl-isolated-on-white-background.jpg",
+    },
+    {
+      ingredientId: 1,
+      name: "Água",
+      photo_url:
+        "https://images.tcdn.com.br/img/img_prod/715985/xicara_em_vidro_para_cha_crystal_260ml_wheaton_8891_1_8c9f9a063f1f5de84edb716ca3697970.jpg",
+    },
+    {
+      ingredientId: 2,
+      name: "Alho",
+      photo_url:
+        "https://image.freepik.com/free-photo/fresh-garlic-white-background_1339-17012.jpg",
+    },
+    {
+      ingredientId: 3,
+      name: "Batata",
+      photo_url:
+        "http://www.valleyspuds.com/wp-content/uploads/Russet-Potatoes-cut.jpg",
+    },
+    {
+      ingredientId: 4,
+      name: "Cebola",
+      photo_url:
+        "https://image.shutterstock.com/image-photo/mixed-spices-isolated-on-white-260nw-662383828.jpg",
+    },
+    {
+      ingredientId: 6,
+      name: "Creme de Leite",
+      photo_url:
+        "https://media.istockphoto.com/photos/mayonnaise-in-bowl-isolated-on-white-background-picture-id614981116?k=6&m=614981116&s=612x612&w=0&h=LtbsI2HQXOTERYuP9YJ2PJfRF3W6DcyZ798fxMcQWC0=",
+    },
+    {
+      ingredientId: 7,
+      name: "Farinha de Trigo",
+      photo_url:
+        "https://images.assetsdelivery.com/compings_v2/seregam/seregam1309/seregam130900036.jpg",
+    },
+    {
+      ingredientId: 8,
+      name: "Gelo",
+      photo_url:
+        "https://www.bebidaboa.com.br/arquivos/noticias/0/33/fotoprincipal/large/33.jpg",
+    },
+    {
+      ingredientId: 9,
+      name: "Ketchup",
+      photo_url:
+        "https://st2.depositphotos.com/5262887/11050/i/950/depositphotos_110501208-stock-photo-ketchup-bowl-isolated-on-white.jpg",
+    },
+    {
+      ingredientId: 10,
+      name: "Leite",
+      photo_url:
+        "https://agrorevenda.com.br/wp-content/uploads/2019/12/leite.png",
+    },
+    {
+      ingredientId: 11,
+      name: "Limão",
+      photo_url: "https://static.significados.com.br/foto/limao-taiti-cke.jpg",
+    },
+    {
+      ingredientId: 12,
+      name: "Manjericão Seco",
+      photo_url:
+        "https://www.honeychop.com/wp-content/uploads/2015/09/Dried-Mint.png",
+    },
+    {
+      ingredientId: 13,
+      name: "Manteiga",
+      photo_url:
+        "https://veja.abril.com.br/wp-content/uploads/2016/05/manteiga-20130523-original2.jpeg?quality=90&strip=info&w=620&h=349&crop=1",
+    },
+    {
+      ingredientId: 14,
+      name: "Molho Picante",
+      photo_url:
+        "https://media.istockphoto.com/photos/opened-can-of-spaghetti-sauce-on-a-white-background-picture-id497704752?k=6&m=497704752&s=612x612&w=0&h=JnL54buYu1Z3fGtd8uNdjFxiAKwlxoDluD6jbIfSaZI=",
+    },
+    {
+      ingredientId: 15,
+      name: "Molho de tomate",
+      photo_url:
+        "https://akdelicatessen.com.br/wp-content/uploads/2021/11/molho-tomate-caseiro.jpg",
+    },
+    {
+      ingredientId: 16,
+      name: "Oleo",
+      photo_url:
+        "https://ak7.picdn.net/shutterstock/videos/27252067/thumb/11.jpg",
+    },
+    {
+      ingredientId: 17,
+      name: "Orégano",
+      photo_url: "https://frutascharito.es/886-large_default/oregano.jpg",
+    },
+    {
+      ingredientId: 18,
+      name: "Paprica",
+      photo_url:
+        "https://image.freepik.com/free-photo/red-chilli-pepper-powder-isolated-white-background_55610-28.jpg",
+    },
+    {
+      ingredientId: 19,
+      name: "Pimenta Verde",
+      photo_url: "https://ak9.picdn.net/shutterstock/videos/4055509/thumb/1.jpg",
+    },
+    {
+      ingredientId: 20,
+      name: "Pimenta Vermelha",
+      photo_url: "https://ak9.picdn.net/shutterstock/videos/10314179/thumb/1.jpg",
+    },
+    {
+      ingredientId: 21,
+      name: "Pó de Pimenta",
+      photo_url:
+        "https://us.123rf.com/450wm/nuttapong/nuttapong1505/nuttapong150500009/40458002-paprika-powder-isolated-on-white-background.jpg?ver=6",
+    },
+    {
+      ingredientId: 22,
+      name: "Queijo",
+      photo_url: "https://ak7.picdn.net/shutterstock/videos/3619997/thumb/1.jpg",
+    },
+    {
+      ingredientId: 23,
+      name: "Sal",
+      photo_url:
+        "https://image.freepik.com/free-photo/sea-salt-wooden-bowl-isolated-white-background_29402-416.jpg",
+    },
+  ];
+  
