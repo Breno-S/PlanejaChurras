df5bee5e3c919e51e353c69ebf72e7a2b309208d
amrithomas <amri.righetti@gmail.com>
2023-09-27 16:27:51 -0300

    Update

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,29 +1,53 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import { StyleSheet, TextInput, View, Text } from "react-native";
 import { useFonts, Karantina_400Regular } from "@expo-google-fonts/karantina";
 
-export default function Input({ type, label }) {
+export default function Input({ type, label, onValueChange  }) {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (text) => {
+    setInputValue(text);
+
+    if (onValueChange) {
+      onValueChange(text);
+    }
+  };
+
   const renderInput = () => {
     switch (type) {
       case "text":
         return (
           <View style={styles.contentInput}>
             <Text style={styles.text}>{label}</Text>
-            <TextInput style={styles.input} />
+            <TextInput
+              style={styles.input}
+              onChangeText={handleInputChange}
+              value={inputValue}
+            />
           </View>
         );
       case "number":
         return (
           <View style={styles.contentInput}>
-            <Text style={styles.text}>Telefone</Text>
-            <TextInput style={styles.input} keyboardType="numeric" />
+            <Text style={styles.text}>{label}</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="numeric"
+              onChangeText={handleInputChange}
+              value={inputValue}
+            />
           </View>
         );
       case "cel":
         return (
           <View style={styles.contentInput}>
             <Text style={styles.text}>{label}</Text>
-            <TextInput style={styles.input} keyboardType="phone-pad" />
+            <TextInput
+              style={styles.input}
+              keyboardType="phone-pad"
+              onChangeText={handleInputChange}
+              value={inputValue}
+            />
           </View>
         );
     }
@@ -34,17 +58,20 @@ export default function Input({ type, label }) {
 
 const styles = StyleSheet.create({
   contentInput: {
-    margin: 10,
+    margin: 20,
   },
   input: {
     borderBottomColor: "#EF820D",
     borderBottomWidth: 1,
     padding: 10,
+    color: "#fff",
+    width: 300,
   },
-  text : {
-    color : "#fff",
+  text: {
+    color: "#fff",
     fontFamily: "Karantina_400Regular",
     fontSize: 24,
-    letterSpacing : 1
-  }
+    letterSpacing: 1,
+    marginBottom: 5,
+  },
 });
