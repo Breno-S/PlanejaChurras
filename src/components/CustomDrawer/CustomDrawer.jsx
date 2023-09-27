700f460be47a0270e022f83d88724964dabd6320
Breno-S <114250412+Breno-S@users.noreply.github.com>
2023-09-27 16:20:32 -0300

    drawer
    tô sem autocomplete nessa joça de VSCode

--- src/components/CustomDrawer/CustomDrawer.jsx
+++ src/components/CustomDrawer/CustomDrawer.jsx
@@ -1,24 +1,48 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image, StatusBar } from "react-native";
 import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
 import { Dimensions } from "react-native";
 
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import Logo from "../../../assets/images/icon-churras.png"
+
+const Icon_Grelha = <Icon name="outdoor-grill" size={30} color="#9A1A1A" />
+const Icon_Home = <Icon name="home" size={30} color="#9A1A1A" />
 
 const windowHeight = Dimensions.get('window').height;
 
 export default function CustomDrawer({ navigation }) {
   return (
     // Fundo do Drawer/ Drawer itself
-    <DrawerContentScrollView contentContainerStyle={{backgroundColor: '#000'}}>
+    <>
+    <StatusBar />
+    <DrawerContentScrollView contentContainerStyle={{flex: 1, backgroundColor: '#191B1B'}}>
+      
       {/* Header do Drawer */}
       <View>
-        <Image source={Logo}/>
-        <Text>""</Text>
+        <Image source={Logo} style={ {width: 180, height: 180, alignSelf: 'center'} }/>
       </View>
 
-	</DrawerContentScrollView>
+      {/* Main do Drawer */}
+      <View style={ {} }>
+      <DrawerItem
+        label={"Home"}
+        labelStyle={ {color: '#fff'} }
+        icon={() => Icon_Home}
+        onPress={() => navigation.navigate("HomePage")}
+      />
 
+      <DrawerItem
+        label={"Novo Churrasco"}
+        labelStyle={ {color: '#fff'} }
+        icon={() => Icon_Grelha}
+        onPress={() => navigation.navigate("NovoChurras")}
+      />
+      
+      </View>
+
+	  </DrawerContentScrollView>
+    </>
   );
 }
 
