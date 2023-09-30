c6758f40d40664d92e0a79f1a162ee3cc5fd01fd
Breno-S <114250412+Breno-S@users.noreply.github.com>
2023-09-30 14:07:18 -0300

    exemplo sqlite
    exemplo desenvolvido em sala, para tomar como base

---
+++ src/services/FuncsDB.jsx
@@ -0,0 +1,104 @@
+import db from "../database";
+
+export function criaTabela() {
+    db.transaction((tx) => {
+        tx.executeSql(
+            `
+                CREATE TABLE IF NOT EXISTS Notas (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    titulo TEXT,
+                    categoria TEXT,
+                    texto TEXT
+                );
+            `,
+            [],
+            (_, error) => {
+                console.log(error);
+            }
+        );
+    });
+}
+
+export async function adicionaNota(nota){
+    return new Promise((resolve, reject) => {
+        db.transaction((tx) => {
+            tx.executeSql(
+                `
+                    INSERT INTO Notas (titulo, categoria, texto) VALUES (?, ?, ?);
+                `,
+                [
+                    nota.titulo,
+                    nota.categoria,
+                    nota.texto
+                ],
+                (_, { rowsAffected, insertId }) => {
+                    if (rowsAffected > 0) resolve(insertId);
+                        else reject('Erro ao adicionar a nota: ' + JSON.stringify(nota));
+                },
+                (_, error) => reject(error)
+            )
+        })
+    })
+}
+
+export async function buscaNotas(categoria ='*') {
+    return new Promise((resolve, reject) => {
+        db.transaction((tx) => {
+            let comando;
+            if(categoria === '*'){
+                comando = 'SELECT * FROM Notas;';
+            } else {
+                comando = `SELECT * FROM Notas WHERE categoria = '${categoria}';`;
+            }
+
+            tx.executeSql(comando, [],
+            (transaction, resultado) => {
+                resolve(resultado.rows._array);
+            },
+            (_, error) => reject(error)
+            )
+        })
+    })
+}
+
+export async function atualizaNota(nota) {
+    return new Promise((resolve, reject) => {
+        db.transaction((tx) => {
+            tx.executeSql(
+                `
+                    UPDATE Notas SET titulo = ?, categoria = ?, texto = ? WHERE ID = ?;
+                                
+                `,
+                [
+                    nota.titulo,
+                    nota.categoria,
+                    nota.texto,
+                    nota.id
+                ],
+                () => {
+                    resolve('Nota atualizada com sucesso!');
+                },
+                (_, error) => reject(error)
+            )
+        })
+    })
+}
+
+export async function removeNota(id) {
+    return new Promise((resolve, reject) => {
+        db.transaction((tx) => {
+            tx.executeSql(
+                `
+                    DELETE FROM Notas WHERE ID = ?;
+                `,
+                [
+                    id
+                ],
+                () => {
+                    resolve('Nota atualizada com sucesso!');
+                },
+                (_, error) => reject(error)
+            )
+        })
+    })
+}
